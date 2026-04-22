ad2aef614fad23f782e29c2972e3e6641b47a179
CONt12006 <162928166+CONt12006@users.noreply.github.com>
2026-04-22 13:23:45 +0300

    Create ship.py

---
+++ ship.py
@@ -0,0 +1,43 @@
+import pygame
+
+class Ship():
+    """Класс для управлением корабля."""
+    def __init__(self,ai_game):
+        """Инициирует корабль и задаёт его начальную позицию."""
+        self.screen=ai_game.screen
+        self.settings=ai_game.settings
+        self.screen_rect=ai_game.screen.get_rect()
+
+        #Загружает изображение корабля и получает прямоугольник.
+        self.image=pygame.image.load('C:/Users/kmkar/CAKE_PROGRAMMER/python/Alien_Invasion/images/ship.bmp')
+        self.image.set_colorkey((255, 255, 255))
+        self.rect=self.image.get_rect()
+        #Каждый новый корабль появляется у нижнего края экрана.
+        self.rect.midbottom=self.screen_rect.midbottom
+
+        # Сохранение вещественной координаты центра коробля.
+        self.x=float(self.rect.x)
+
+        # Флаги перемещения
+        self.moving_right=False
+        self.moving_left=False
+
+    def update(self):
+        """Обновляет позицию корабля с учётом флагов."""
+        # Обноволяется атрибут x, не rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Обновление атрибута rect на основании self.x.
+        self.rect.x=self.x
+
+    def blitme(self):
+        """Рисует корабль в текущей позиции."""
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Размещает корабль в центре нижней стороны."""
+        self.rect.midbottom=self.screen_rect.midbottom
+        self.x=float(self.rect.x)
