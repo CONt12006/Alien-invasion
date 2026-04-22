35d664170bbeae509e28e1c45717cddc46d17ae8
CONt12006 <162928166+CONt12006@users.noreply.github.com>
2026-04-22 13:23:34 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,29 @@
+class Settings():
+    """Класс для хранения всех настроек игры Alien Invasion"""
+
+    def __init__(self):
+        """Инициирует настройки игры."""
+        #Параметры экрана
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color=(230,230,230)
+
+        # Настройки корабля 
+        self.ship_speed=1.5
+        self.ship_limit=3
+
+        # Параметры снаряда
+        self.bullet_speed=1.5
+        self.bullet_width=3
+        self.bullet_height=15
+        self.bullet_color=(60,60,60)
+        self.bullets_allowed=3
+
+        # Настройки пришельцев 
+        self.alien_speed=0.5
+        self.fleet_drop_speed=200
+        # fleet_direction=1 обозначает движение вправо; а -1 - влево
+        self.fleet_direction=1
+
+        # Подсчёт очков
+        self.alien_points=50
